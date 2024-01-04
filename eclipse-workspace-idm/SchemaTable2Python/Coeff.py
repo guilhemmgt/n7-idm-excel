efe664c9bebb7524bb26a6b351bf7c73063226e9
Claire0607 <clairescheyder1@gmail.com>
2024-01-04 15:14:49 +0100

    Initialisation tableau python (acceleo)

---
+++ eclipse-workspace-idm/SchemaTable2Python/Coeff.py
@@ -0,0 +1,10 @@
+class Coeff:
+
+	def __init__(self):
+		self.table = [[]]  # Initialisation du tableau
+		# Initialisation de la première ligne avec le nom des colonnes
+		self.table[0]=[Examen]
+
+	def load(self, csv_file):
+		
+
