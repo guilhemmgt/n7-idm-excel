63f395552fd2eb79470b0c1d3f4958bbb7c92115
Guilhem <guilhem.mangenot@gmail.com>
2024-01-14 12:00:15 +0100

    outils sirius SchemaTable

--- eclipse-workspace-idm/SchemaTable/src-gen/schemaTable/impl/ContenuColonneImpl.java
+++ eclipse-workspace-idm/SchemaTable/src-gen/schemaTable/impl/ContenuColonneImpl.java
@@ -5,11 +5,9 @@ package schemaTable.impl;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.NotificationChain;
 import org.eclipse.emf.ecore.EClass;
-
 import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
-
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import schemaTable.Colonne;
 import schemaTable.ContenuColonne;
